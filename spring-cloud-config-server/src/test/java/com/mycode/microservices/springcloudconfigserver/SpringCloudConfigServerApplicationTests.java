519703cddfdbc8539b81aff7e611e8ed1c23d32f
Sudhasrinivas Pallam <pallam.sudhasrinivas@gmail.com>
2021-07-09 07:32:45 +0530

    Spring config server
    Spring config server

---
+++ spring-cloud-config-server/src/test/java/com/mycode/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycode.microservices.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
